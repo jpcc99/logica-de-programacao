dfbe17ab60d70a9dd8c8ac25a01bf31ff7ff7716
jpcc99 <jpcc.dev@gmail.com>
2024-12-17 19:22:20 -0300

    Adds class08: lists

---
+++ 08.listas/exemplo1.py
@@ -0,0 +1,17 @@
+numbers = [11, 22, 33, 44, 55]
+
+"""
+print("Forma 1: (todos juntos)")
+print(numbers)
+"""
+
+"""
+print("Forma 2: (um embaixo do outro)")
+for n in numbers:
+    print(n)
+"""
+
+print("Forma 3: (lado a lado)")
+for n in numbers:
+    print(n, end=" ")
+print()
